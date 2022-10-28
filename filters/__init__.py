d14dbe414cb6c0c07b8cd8b5b77a2a6a3c046c2d
Rishu Pandey <55981293+theRishu@users.noreply.github.com>
2022-10-28 08:11:54 -0700

    Update __init__.py

--- filters/__init__.py
+++ filters/__init__.py
@@ -3,7 +3,18 @@ from aiogram import Dispatcher
 from loader import dp
 # from .is_admin import AdminFilter
 
+from . import animation
+from . import audio
+from . import document
+from . import photo
+from . import sticker
+from . import location
+from . import video
+from . import voice
+
+
+
 
 if __name__ == "filters":
-    # dp.filters_factory.bind(AdminFilter)
+    #dp.filters_factory.bind(is_admin)
     pass
