f77b9f4b80b7d46955d4150c11cb417af4db2067
Rishu Pandey <55981293+theRishu@users.noreply.github.com>
2022-11-05 14:28:16 -0700

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,6 +3,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.MARKDOWN)
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
