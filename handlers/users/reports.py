4b3fb69002cb04d7e40c80bb3fe09ee11fd5f81c
Rishu Pandey <55981293+theRishu@users.noreply.github.com>
2022-11-16 21:38:43 +0530

    Update reports.py

--- handlers/users/reports.py
+++ handlers/users/reports.py
@@ -1,1 +1,19 @@
 
+from loader import dp, bot
+import constant
+from aiogram import types
+from utils.misc import db_commands as db
+from aiogram.types import CallbackQuery, ContentType, Message, reply_keyboard
+from keyboards.inline.reaccept_button.reaccept_cancel import cancel_button
+from keyboards.inline.reaccept_button.reopen_button import reopen_keyboard
+from keyboards.inline.stop_searching import stop_search
+
+
+
+FC = '-846814382'
+
+@dp.message_handler(commands="report"):
+async def report(message: types.Message):
+    
+
+    
